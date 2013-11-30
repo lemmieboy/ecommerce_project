65f533ad91ce158ab45df5a30fcbc24cfd907f12
Lemuel Volante <lemuel.volante@gmail.com>
2013-11-30 11:39:46 -0600

    Added partials to the project

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,10 @@
 class ProductsController < ApplicationController
   def index
+  	 @products = Product.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @products }
+    end
   end
 end
