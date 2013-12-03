b3b4a53496ddc6ff93c87711271e4b9986a20675
Lemuel Volante <lemuel.volante@gmail.com>
2013-12-02 19:07:10 -0600

    Added dropdown list for category in rails program

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,20 @@
 class ProductsController < ApplicationController
   def index
-  	 @products = Product.all
+  	@products = Product.all
+    @categories = Category.all
 
-    # respond_to do |format|
-    #   format.html # index.html.erb
-    #   format.json { render json: @products }
-    # end
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @products }
+    end
   end
+
 
   def search_results
   	# Here we will be using the product model to actually search
+    @categories = Category.all
     @products = Product.where("name LIKE ?", "%#{params[:keywords]}%")
+
   end 
 
 end
