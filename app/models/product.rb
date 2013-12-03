02aa63549b09c0963c4718cef73b389cbc075f3a
Lemuel Volante <lemuel.volante@gmail.com>
2013-12-03 17:01:16 -0600

    fixed error in active admin (it can now delete data)

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
   attr_accessible :category_id, :description, :image_filename, :name, :price
 
   belongs_to :category
+  has_many :line_items
   has_many :orders, :through => :line_items
   
   validates :description, :name, :price, :presence => true
