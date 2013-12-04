00a73567a2913f8127d16e563b4e8a8784a96b3f
Florida Elago <florida.elago@gmail.com>
2013-12-03 18:19:05 -0600

    merge

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,5 +1,9 @@
 //= require jquery
 //= require jquery.ui.all
 //= require jquery_ujs
+<<<<<<< HEAD
 
 //= require active_admin/application 
+=======
+//= require active_admin/application
+>>>>>>> 02aa635... fixed error in active admin (it can now delete data)
